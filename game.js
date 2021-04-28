64b14ce87882d83111e97be12278dc3aef0a8f3e
root <marisaarevali@gmail.com>
2021-04-28 13:45:40 +0200

    parandus2

--- game.js
+++ game.js
@@ -24,13 +24,13 @@ var game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('uuesti', 'assets/uuesti.png');
-    this.load.image('manglabi', 'assets/manglabi.png');
+    this.load.image('m2ngl2bi', 'assets/manglabi.png');
 
     this.load.spritesheet('munaseisab', 'assets/munaseisab.png', { frameWidth: 53, frameHeight: 95 })
     this.load.spritesheet('munajookseb', 'assets/munajookseb3.png', { frameWidth: 90, frameHeight: 95 })
     this.load.spritesheet('munakatki', 'assets/munakatki.png', { frameWidth: 53, frameHeight: 95 })
 
-    this.load.image('maapind', 'assets/maapind3.png');
+    this.load.image('maapind', 'assets/maapind4.png');
 
     this.load.image('takistus1', 'assets/takistus1.png')
     this.load.image('takistus2', 'assets/takistus2.png')
@@ -41,39 +41,39 @@ function preload() {
 
 const { height, width } = this.game.config;
 
-// Siin all deklareerime mängu muutujad
-
+// Siin deklareerime mängu muutujad
 var maapind;
 var tegelane;
 // klaviatuuri sisendi muutuja
-var cursors;
-var manguKiirus;
+var klaviatuur;
+var m2nguKiirus;
+var deltaKiirus;
 // Boolean muutuja, kui väärtus on false siis mäng seisab
 // vajutades tühikut või hiire klikki muutub väärtus true'ks ja mäng läheb käima
-mang = false;
+m2ngK2ib = false;
 //takistuste list
 takistusedList = ['takistus1', 'takistus2', 'takistus3', 'takistus4', 'takistus5'];
 // Takistuse pildi muutuja
 var takistus;
 // Takistuste tekke aeg
-var sunniAeg = 0;
+var synniAeg = 0;
 
 
 
 // skoor
 skoor = 0;
-// Highscore hoiame localstorages, et peale brauseri uuendamist oleks see alles.
+// Kõrgeimat skoori hpiame brauseri mälus, et peale brauseri uuendamist oleks see alles.
 // Kuna localstorage hoiab endas väärtusi sõnena, siis teisendame väärtuse täisarvuks
-// Kuna esimesel kasutusel pärib mäng localstoragest Highskoori ja saab vastuseks puuduva väärtuse,
+// Esimesel kasutusel pärib mäng mälust skoori ja saab vastuseks puuduva väärtuse,
 // siis asendame selle arvuga 0
 if (localStorage.getItem('skoor') == null) {
-    highSkoor = 0;
+    maksSkoor = 0;
 } else {
-    highSkoor = parseInt(localStorage.getItem('skoor'));
+    maksSkoor = parseInt(localStorage.getItem('skoor'));
 }
 // Boolean muutuja mis on false kui mäng käib
 // ja muutub true'ks kui vastu takistust minna
-gameOver = false;
+kasKaotasid = false;
 
 
 function create() {
@@ -84,7 +84,7 @@ function create() {
     maapind = this.add.tileSprite(0, height, width, 20, 'maapind').setOrigin(0, 1);
 
     // Kiirus, millega mäng algab
-    manguKiirus = 10;
+    m2nguKiirus = 10;
 
 
     // (x-telje asukoht,y-telje asukoht,kõrgus,spraidi nimetus)
@@ -106,51 +106,56 @@ function create() {
     takistused = this.add.group();
     //skoor tekst
     skooriKiri = this.add.text(width - 300, 50, 'Skoor: 0', { fill: '#000', font: '700 24px Arial' });
-    //highcsore 
-    highSkooriKiri = this.add.text(width - 300, 0, 'Kõrgeim skoor: ' + highSkoor, { fill: '#000', font: '700 24px Arial' });
+    //Kõrgeim skoor
+    maksSkoorKiri = this.add.text(width - 300, 0, 'Kõrgeim skoor: ' + maksSkoor, { fill: '#000', font: '700 24px Arial' });
 
-    //mang labi pilt, setAlpha on kõikidel elementidel default 1 mis on nähtav. 0 väärtus on läbipaistev
-    mangLabi = this.add.container(width / 2, height / 2).setAlpha(0);
-    mangLabiTekst = this.add.image(0, 0, 'manglabi');
+    //mäng läbi pilt, setAlpha on kõikidel elementidel default 1 mis on nähtav. 0 väärtus on läbipaistev
+    m2ngL2bi = this.add.container(width / 2, height / 2).setAlpha(0);
+    m2ngL2biTekst = this.add.image(0, 0, 'm2ngl2bi');
     //setInteractive teeb selle pildi klikitavaks
     uuesti = this.add.image(0, 120, 'uuesti').setInteractive();
 
-    mangLabi.add([
-        mangLabiTekst,
+    m2ngL2bi.add([
+        m2ngL2biTekst,
         uuesti
     ])
     //klaviatuuri sisend
-    cursors = this.input.keyboard.createCursorKeys();
+    klaviatuur = this.input.keyboard.createCursorKeys();
 
     // kui tegelane jookseb takistuse pihta siis käivita see funktsioon
     this.physics.add.collider(tegelane, takistused, function kokku() {
         //peata mäng
-        mang = false;
-        //mängläbi ?
-        gameOver = true;
+        m2ngK2ib = false;
+        //kas kaotasid?
+        kasKaotasid = true;
         //muuda tegelase tekstuuri
         tegelane.setTexture('munakatki');
         //resetime mängukiiruse
-        manguKiirus = 10;
+        m2nguKiirus = 10;
         //resetime takistuste tekkeaja
-        sunniAeg = 0;
-        //manglabi container nähtavaks
-        mangLabi.setAlpha(1);
+        synniAeg = 0;
+        //Mäng läbi container nähtavaks
+        m2ngL2bi.setAlpha(1);
+        if (skoor > maksSkoor) {
+            maksSkoor = skoor;
+            maksSkoorKiri.setText('Kõrgeim skoor: ' + maksSkoor).setAlpha(1);
+        }
+        localStorage.setItem('skoor', maksSkoor);
     });
 
     // Skoori arvutamise ja mängukiiruse suurendamine
     this.time.addEvent({
-        delay: 1000 / 10, //10 korda sekudis kutsutakse välja
+        delay: 1000 / 10, //10 korda sekundis kutsutakse välja
         loop: true, // kutsu seda funktsiooni välja lõputult
         callbackScope: this,
         //mida teeb funktsioon kui teda välja kutsutakse
         callback: () => {
-            // Ka mäng käib?
-            if (!mang) { return; } //kui ei siis ära siit edasi mine
+            // Kas mäng käib?
+            if (!m2ngK2ib) { return; } //kui ei siis ära siit edasi mine
             // tõsta skoori 1 võrra, 10 korda sekundis
             skoor++
             // Lisame mängukiirusele 0.01
-            manguKiirus += 0.01;
+            m2nguKiirus += 0.01;
             skooriKiri.setText('Skoor: ' + skoor);
         }
     })
@@ -161,40 +166,28 @@ function create() {
         //kustutab kõik takistused mängust ja grupis - default value on false,false
         takistused.clear(true, true);
         //nuppu vajutades mäng uuesti käima
-        mang = true;
-        gameOver = false;
+        m2ngK2ib = true;
+        kasKaotasid = false;
         // Varjame jälle mäng läbi konteineri
-        mangLabi.setAlpha(0);
+        m2ngL2bi.setAlpha(0);
         // taastame alge mängukiiruse
-        manguKiirus = 10;
-        //vajadusel uuendame highscore
-        if (skoor > highSkoor) {
-            highSkoor = skoor;
-            highSkooriKiri.setText('Kõrgeim skoor: ' + highSkoor).setAlpha(1);
-
-        }
-
-        // paneme highskoori local datasse
-        localStorage.setItem('skoor', highSkoor);
-        //resetime skoori
+        m2nguKiirus = 10;
         skoor = 0;
 
     })
-    // 
-    pointer = this.input.activePointer;
+    //hiireklikk
+    hiireKlikk = this.input.activePointer;
 
-/*     this.input.on('pointerdown', function(){
-        tegelane.setVelocityY(-1800);
-    }, this);
- */
 }
 
 
 
 function update(time, delta) {
 
+
+
     // kui space vajutad, siis tee seda
-    if (cursors.space.isDown && mang == false && gameOver == false || pointer.isDown && mang == false && gameOver == false) {
+    if (klaviatuur.space.isDown && m2ngK2ib == false && kasKaotasid == false || hiireKlikk.isDown && m2ngK2ib == false && kasKaotasid == false) {
         //kui tegelane ei ole vastu maad siis ära hüppa
         if (!tegelane.body.onFloor()) { return; }
         tegelane.setVelocityY(-1800);
@@ -205,7 +198,7 @@ function update(time, delta) {
         // lisame eventi mille käigus hakkab muna jooksma
         //taust hakkab liikuma
         // ja muna asukoht tuleb mängu äärest eemale    
-        var startMang = this.time.addEvent({
+        var startM2ng = this.time.addEvent({
             //delay: 1000/70,
             loop: true,
             callbackScope: this,
@@ -216,26 +209,30 @@ function update(time, delta) {
 
                 if (tegelane.x > 100) {
                     //see paneb update funktsiooni sisu lõpuks käima
-                    mang = true;
+                    m2ngK2ib = true;
                     //tegelane ei liigu enam edasi, sest taust liigub
                     tegelane.setVelocity(0);
                     // Paneme tegelase liikuma
                     tegelane.play('jooksmine', 1);
-                    startMang.remove();
+                    startM2ng.remove();
                 }
 
             }
         })
     }
 
-    if (mang == false) { return; }
-    maapind.tilePositionX += manguKiirus;
+    if (m2ngK2ib == false) { return; }
 
-    //suurendame takistuste asukohta mangujooksul
-    Phaser.Actions.IncX(takistused.getChildren(), -manguKiirus);
+    // Ühtlase liikumise loomiseks korrutame mängukiiruse deltaga läbi
+    deltaKiirus = m2nguKiirus * delta * 0.06;
+    //maapinna liikumine
+    maapind.tilePositionX += deltaKiirus;
+    //suurendame takistuste asukohta mängu jooksul
+    Phaser.Actions.IncX(takistused.getChildren(), -deltaKiirus);
 
-    // Kui vajutad tühikut ja tegelane on maas // kui teed hiirekliki ja tegelane on maas
-    if (cursors.space.isDown && tegelane.body.onFloor() || pointer.isDown && tegelane.body.onFloor()) {
+
+    // Kui vajutad tühikut ja tegelane on maas või kui teed hiirekliki ja tegelane on maas
+    if (klaviatuur.space.isDown && tegelane.body.onFloor() || hiireKlikk.isDown && tegelane.body.onFloor()) {
         tegelane.setVelocityY(-1800);
     }
     //kui tegelase y väärtus on suurem kui 0, peata liikumise animatsioon ja pane ta paigale (sest ta on õhus)
@@ -247,21 +244,21 @@ function update(time, delta) {
     };
 
     //paneme takistuste sünniaja jooksma
-    sunniAeg += delta * manguKiirus * 0.08;
+    synniAeg += delta * m2nguKiirus * 0.08;
 
     //kui sünniaeg on suurem kui 1500 siis pane takistusefunktsioon tööle
     //ja nulli ära sünniaeg
-    if (sunniAeg >= 1500) {
+    if (synniAeg >= 1500) {
         //loome random numbri 0 - 4
         var takistuseNumber = Math.floor(Math.random() * 5);
         // vahemaa mille vahel takistusi loome
         var vahemaa = Phaser.Math.Between(600, 1200);
         //takistuseFunk();
         // loome takistused teele
-        takistus = this.physics.add.sprite(width + vahemaa, height, takistusedList[takistuseNumber]).setOrigin(0, 1).setImmovable();
+        takistus = this.physics.add.sprite(width + vahemaa, height, takistusedList[takistuseNumber]).setImmovable().setOrigin(0, 1);
         takistused.add(takistus);
         console.log(takistuseNumber);
-        sunniAeg = 0;
+        synniAeg = 0;
     }
 
 
